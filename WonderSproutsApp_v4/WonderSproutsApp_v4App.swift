60d83aefecd73f2abf3a2f34b0e67f915aebf0f8
Charles Atchison <Charles.Atchison@outlook.com>
2023-07-20 18:51:11 -0500

    Initial Commit

---
+++ WonderSproutsApp_v4/WonderSproutsApp_v4App.swift
@@ -0,0 +1,17 @@
+//
+//  WonderSproutsApp_v4App.swift
+//  WonderSproutsApp_v4
+//
+//  Created by Charles Atchison on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct WonderSproutsApp_v4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
