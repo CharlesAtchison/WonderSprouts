9a180c87a2ac5b241cc60deead0092408f2e0f47
Charles Atchison <Charles.Atchison@outlook.com>
2023-07-20 21:53:59 -0500

    Added Back button and Build Story Buttons

--- WonderSproutsApp_v4/StoryBuilderView.swift
+++ WonderSproutsApp_v4/StoryBuilderView.swift
@@ -12,7 +12,7 @@ struct StoryBuilderView: View {
     @State private var selectedWhat: String?
     @State private var selectedWhere: String?
     @State private var selectedWhen: String?
-
+    
     var selections: [(text: String, view: AnyView, binding: () -> Binding<String?>)] {
         [
             ("Who is going with you?", AnyView(WhoView(selected: selectedWhoBinding)), { self.selectedWhoBinding }),
@@ -21,41 +21,70 @@ struct StoryBuilderView: View {
             ("When are you going?", AnyView(WhenView(selected: selectedWhenBinding)), { self.selectedWhenBinding })
         ]
     }
-
+    
     var selectedWhoBinding: Binding<String?> { $selectedWho }
     var selectedWhatBinding: Binding<String?> { $selectedWhat }
     var selectedWhereBinding: Binding<String?> { $selectedWhere }
     var selectedWhenBinding: Binding<String?> { $selectedWhen }
-
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
     var body: some View {
-        NavigationView {
-            ZStack {
-                Image("background_image")
-                    .resizable()
-                    .ignoresSafeArea()
-
-                VStack(spacing: 20) {
-                    ForEach(selections, id: \.text) { item in
-                        NavigationLink(destination: item.view) {
-                            Text(item.binding().wrappedValue ?? item.text)
-                                .customTextStyle(selectedValue: item.binding().wrappedValue, value: item.text)
+            NavigationView {
+                ZStack {
+                    Image("background_image")
+                        .resizable()
+                        .ignoresSafeArea()
+                    
+                    // Main VStack containing the NavigationLinks
+                    VStack(spacing: 20) {
+                        ForEach(selections, id: \.text) { item in
+                            NavigationLink(destination: item.view) {
+                                Text(item.binding().wrappedValue ?? item.text)
+                                    .customTextStyle(selectedValue: item.binding().wrappedValue, value: item.text)
+                            }
                         }
                     }
-                }
-                .padding()
-                .background(
+                    .padding()
+                    
+                    // VStack just for the title, positioned at the very top
                     VStack {
                         Text("Story Builder")
                             .font(.title)
-                            .padding()
-                        Spacer()
+                            .padding(.top, 5) // Adjust to move the title closer or further from the top
+                        Spacer() // Push the title to the top
                     }
-                )
+                    
+                    // VStack to push the HStack with buttons to the bottom
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Button(action: {
+                                // Navigate back to ContentView
+                                self.presentationMode.wrappedValue.dismiss()
+                            }) {
+                                Text("Back")
+                                    .modifier(ButtonModifier()) // Assuming you have ButtonModifier similar to CustomPickerView
+                            }
+                            .padding(.leading, 20)
+                            
+                            Spacer()
+                            
+                            Button(action: {
+                                // Implement your build story action here
+                            }) {
+                                Text("Build Story!")
+                                    .modifier(ButtonModifier()) // Assuming you have ButtonModifier similar to CustomPickerView
+                            }
+                            .padding(.trailing, 20)
+                        }
+                        .padding(.bottom, 20)
+                        
+                    }
+                }.edgesIgnoringSafeArea(.bottom)
             }
         }
     }
-}
-
 
 struct WhoView: View {
     @Binding var selected: String?
