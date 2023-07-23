125f01e6504d1816a2125a64353a6abf13fe5174
Charles Atchison <Charles.Atchison@outlook.com>
2023-07-23 11:17:16 -0500

    Back button stops audio

--- WonderSproutsApp_v4/StoryView.swift
+++ WonderSproutsApp_v4/StoryView.swift
@@ -43,32 +43,34 @@ class Debouncer {
 struct StoryView: View {
     @State private var currentPage: Int = 0
     @State private var attributedContent: NSAttributedString = NSAttributedString()
+    @Environment(\.presentationMode) var presentationMode
     @ObservedObject var speaker = Speak()
-
+    
     private var backDebouncer = Debouncer(debounceTime: 0.3)
     private var nextDebouncer = Debouncer(debounceTime: 0.3)
-
+    
     var body: some View {
+        NavigationStack {
             ZStack {
                 Image("background_image")
                     .resizable()
                     .ignoresSafeArea()
-
+                
                 VStack(spacing: 20) {
                     PageContentView(text: defaultStoryPages[currentPage], speaker: speaker, currentPage: $currentPage)
                         .id(currentPage)
                         .transition(.scaleAndSlide)
-
+                    
                     HStack(spacing: 60) {
                         // Back Button
                         if currentPage > 0 {
                             Image(systemName: "arrow.left.circle.fill")
-                                    .resizable()
-                                    .frame(width: 40, height: 40)
-                                    .foregroundColor(Color("buttonColor"))
-                                    .background(Color("selectedButtonColor"))
-                                    .clipShape(Circle())
-                                    .onTapGesture {
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(Color("buttonColor"))
+                                .background(Color("selectedButtonColor"))
+                                .clipShape(Circle())
+                                .onTapGesture {
                                     backDebouncer.callback = {
                                         withAnimation {
                                             if self.currentPage > 0 {
@@ -91,7 +93,7 @@ struct StoryView: View {
                                 .frame(width: 40, height: 40)
                                 .opacity(0)
                         }
-
+                        
                         // Play/Pause Button
                         Button(action: {
                             speaker.toggleSpeech(text: defaultStoryPages[currentPage])
@@ -104,16 +106,16 @@ struct StoryView: View {
                                 .background(Color("selectedButtonColor"))
                                 .cornerRadius(20) // Add corner radius for the button
                         }
-
+                        
                         // Next Button
                         if currentPage < defaultStoryPages.count - 1 {
                             Image(systemName: "arrow.right.circle.fill")
-                                    .resizable()
-                                    .frame(width: 40, height: 40)
-                                    .foregroundColor(Color("buttonColor"))
-                                    .background(Color("selectedButtonColor"))
-                                    .clipShape(Circle())
-                                    .onTapGesture {
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(Color("buttonColor"))
+                                .background(Color("selectedButtonColor"))
+                                .clipShape(Circle())
+                                .onTapGesture {
                                     nextDebouncer.callback = {
                                         withAnimation {
                                             if self.currentPage < defaultStoryPages.count - 1 {
@@ -138,7 +140,7 @@ struct StoryView: View {
                         }
                     }
                     .padding(.bottom, 10)
-
+                    
                     Slider(value: $speaker.rate, in: 0.1...1.0, step: 0.1) {
                         Text("Rate")
                     }
@@ -148,9 +150,29 @@ struct StoryView: View {
                 .onChange(of: speaker.currentHighlightedRange, perform: { range in
                     attributedContent = speaker.getAttributedText(for: defaultStoryPages[currentPage])
                 })
+                .navigationBarBackButtonHidden(true)
+                .navigationBarItems(leading: customBackButton(speaker: speaker, presentationMode: presentationMode))
+
             }
         }
     }
+}
+    
+func customBackButton(speaker: Speak, presentationMode: Binding<PresentationMode>) -> some View {
+    Button(action: {
+        // Stop the audio when the back button is pressed
+        speaker.stopSpeaking()
+        // Navigate back to the previous view
+        presentationMode.wrappedValue.dismiss()
+    }) {
+        HStack {
+            Image(systemName: "arrow.left") // Choose your desired image
+            Text("Back")
+        }
+    }
+}
+
+
 
 struct PageContentView: View {
     let text: String
@@ -198,70 +220,66 @@ struct StoryView_Previews: PreviewProvider {
 
 let defaultStoryPages: [String] = [
     // Page 1
-    """
-    *Once upon a time*, in the mystical land of Camelot, there was a legendary king named *Arthur*. He was known far and wide for his wisdom, valor, and his famous sword *Excalibur*, which he pulled from a stone, proving his right to rule. The kingdom prospered under King Arthur's rule, and he was loved by his subjects. However, not everything was peaceful in Camelot. There were rumors of a traitor among the knights of the Round Table, which troubled Arthur greatly. One day, a mysterious sorceress named *Morgana* appeared at the court, claiming to be an ally. Arthur was skeptical, but he couldn't deny the powerful magic she possessed. As time went on, Morgana's intentions became unclear, and the kingdom's peace hung in the balance.
-    """,
-
+    "Once upon a time, in a vast African savannah, there was a young giraffe named Gigi. With her long, slender neck, Gigi could reach the tippy-top branches of the tallest acacia trees. But more than munching on leaves, Gigi loved gazing up at the sky, especially the moon. She often wondered what it might feel like to dance among the stars and touch the radiant moon.",
+    
     // Page 2
-    """
-    In the second chapter of the tale, darkness loomed over Camelot. King Arthur and his loyal knights embarked on a quest to find the legendary *Holy Grail*, a mystical artifact said to grant eternal life and restore harmony to the kingdom. The journey was perilous, filled with treacherous landscapes and dangerous foes. Along the way, Arthur faced many challenges that tested his courage and leadership. Despite the odds, he and his knights persevered, fueled by the hope of a brighter future. As they neared the Holy Grail, Morgana's true intentions were revealed. She sought the Grail's power for herself, intending to use it to conquer Camelot and plunge the land into darkness. Arthur knew he had to stop her at all costs.
-    """,
-
+    "Every night, Gigi would lie down on a hill, her large, dreamy eyes reflecting the silver moonlight. The other animals thought she was daydreaming, but Gigi had a secret wish – to visit the moon. 'How wonderful it would be to leap over the craters and play hide-and-seek behind the moon rocks!' she thought.",
+    
     // Page 3
-    """
-    A climactic battle ensued between Arthur and Morgana, with magic clashing against steel. In the end, Arthur's bravery and righteousness proved stronger, and he defeated Morgana, banishing her from the kingdom forever. With the threat of Morgana gone, King Arthur and his knights finally reached the Holy Grail's resting place. It glowed with a divine light, confirming their quest's righteousness. The artifact was used to heal the land, bringing prosperity and unity back to Camelot. The tale of King Arthur's quest and triumph spread throughout the land, becoming a symbol of hope and justice. To this day, his legend lives on, inspiring generations with his bravery, leadership, and the belief that good will always prevail over evil.
-    """,
-
+    "One day, a wise old owl named Oliver overheard Gigi’s moonlit musings. Oliver had traveled far and wide and heard tales of magical rockets that could take anyone to the moon. 'Why not build a rocket?' he suggested to Gigi with a twinkle in his eye.",
+    
     // Page 4
-    """
-    But peace never lasts forever. As time passed, a new threat emerged, casting its dark shadow upon Camelot. A powerful warlord named Mordred, believed to be Arthur's own son, rose to challenge his father's rule. Mordred was fueled by jealousy and greed, seeking to claim the throne for himself. Arthur faced the heart-wrenching reality of having to confront his own flesh and blood. He knew that the fate of Camelot depended on him putting an end to Mordred's treachery. The final confrontation between father and son was a clash of emotions as much as it was a battle of swords. Mordred was cunning and ruthless, but Arthur's love for his kingdom and people gave him the strength to stand firm.
-    """,
-
+    "Thrilled by the idea, Gigi teamed up with her friends: Timmy the Tortoise, who was a brilliant engineer, and Penny the Parrot, a crafty gatherer. Together, they collected materials and began building their very own moon-bound rocket.",
+    
     // Page 5
-    """
-    In the end, Arthur emerged victorious, but at a terrible cost. The wounds of the battle ran deep, and the land mourned the loss of both a king and a prince. As Arthur lay dying, he entrusted his loyal knight, Sir Bedivere, with the task of returning Excalibur to the Lady of the Lake. With a heavy heart, Sir Bedivere fulfilled the king's wish and cast the sword back into the waters. As the sword disappeared beneath the surface, a hand reached up from the lake to catch it. The Lady of the Lake had received Excalibur once again, signifying the end of Arthur's reign and the passing of an era.
-    """,
-
+    "After many days and nights of hard work, the rocket was ready. Painted in vibrant colors and adorned with twinkling stars, it stood tall, waiting for its maiden voyage. Gigi, with her friends by her side, climbed aboard, eager for the journey ahead.",
+    
     // Page 6
-    """
-    But legends never truly die. Some say that Arthur's body was taken to the mystical isle of Avalon, where he rests, ready to awaken in Britain's hour of need. And so, the tale of King Arthur lives on, immortalized in songs and stories, inspiring countless generations to come. Camelot's legacy endures, a beacon of hope in dark times, a reminder of the extraordinary potential that lies within each of us. And while the knights of the Round Table have scattered, their code of chivalry and honor lives on in the hearts of those who dare to dream and strive for a better world.
-    """,
-
+    "The rocket soared into the night, leaving a trail of sparkles. As they ascended higher, the vast savannah became a distant patchwork quilt. Soon, the blue sky turned to inky black, and the stars shone brighter than ever.",
+    
     // Page 7
-    """
-    The end... or perhaps just the beginning of a new chapter in the story of Camelot. For as long as there are those who believe in the ideals of King Arthur, the legend will continue to shape the destiny of the realm. And so, the tale of King Arthur lives on, weaving its magic through the tapestry of time, forever and always. Thus concludes the epic tale of King Arthur, a saga that will be retold and cherished by generations to come.
-    """,
-
+    "After what seemed like hours, they finally landed with a soft thud. Gigi stepped out and was amazed by the vast lunar landscape. The ground was dusty and dotted with craters. The Earth looked like a blue marble in the distant sky.",
+    
     // Page 8
-    """
-    And so, the legacy of King Arthur and Camelot lives on, an eternal flame of hope that burns brightly in the hearts of those who hear the story. Let it serve as a reminder that within each of us lies the potential for greatness, and that the values of courage, loyalty, and justice will always prevail. As the story of King Arthur and Camelot comes to an end, the echoes of their adventures will resound throughout the annals of history.
-    """,
-
+    "While exploring, Gigi stumbled upon a peculiar sight: a shimmering alien with three eyes and a shiny, green body. The alien introduced himself as Alaric. He had been living on the moon for centuries, observing Earth and its wonders.",
+    
     // Page 9
-    """
-    And thus, the tale of King Arthur lives on, inspiring hearts and minds across the ages, a testament to the enduring power of legends. Though the pages of this story may close, the legend of King Arthur and his knights will forever remain alive in the hearts of those who believe.
-    """,
-
+    "Alaric showed Gigi around, introducing her to the wonders of the moon. They leapt over giant craters, played with moon rocks, and even had a picnic with lunar ice cream, which tasted like nothing Gigi had ever eaten before.",
+    
     // Page 10
-    """
-    For as long as there are storytellers and dreamers, the legacy of King Arthur and Camelot will continue to shine bright. And so, the epic tale of King Arthur and Camelot comes to an end, but its magic will forever linger in the hearts of all who hear it.
-    """,
-
+    "As they spent more time together, Gigi and Alaric shared stories of their homes. Gigi spoke of the vast savannah, the animals, and the warm sun. Alaric told tales of galaxies beyond, of cosmic dances, and stars that sang.",
+    
     // Page 11
-    """
-    And so, the legend of King Arthur and Camelot will live on, inspiring the hearts of those who seek truth, justice, and nobility. As the story of King Arthur and Camelot reaches its conclusion, may it remind us all that legends never truly die.
-    """,
-
+    "One day, Alaric took Gigi to a special place: a hidden valley where stars would come down to rest. The valley was filled with lights, music, and colors that danced around. It was the most magical sight Gigi had ever seen.",
+    
     // Page 12
-    """
-    The tale of King Arthur and Camelot may end, but the spirit of chivalry and honor will endure in the hearts of all who cherish it. And so, the story of King Arthur and Camelot draws to a close, leaving behind a legacy that will inspire generations to come.
-    """,
-
+    "Gigi's stay on the moon was filled with magical moments, but she soon began to miss her home. She missed the golden sunrise, the rustling leaves, and the familiar sounds of the savannah. Sensing her homesickness, Alaric had an idea.",
+    
     // Page 13
-    """
-    But fear not, for the spirit of King Arthur and the ideals of Camelot will forever live on, guiding us through the pages of our own stories, lighting the way to a better world.
-    """
+    "He gave Gigi a small, glowing moonstone. 'Whenever you miss the moon, hold this stone, and it'll show you its magic,' Alaric said with a smile. It was a gift of friendship, a token to remember their time together.",
+    
+    // Page 14
+    "The day came for Gigi to return home. With a heavy heart, she said her goodbyes to Alaric and the moon. The rocket's engines roared to life, and they began their descent back to Earth.",
+    
+    // Page 15
+    "When they landed back on the savannah, a crowd of animals greeted them with cheers and excitement. Everyone was eager to hear about Gigi’s adventures. She narrated tales of the moon's magic, the alien friend she made, and the wonders she'd seen.",
+    
+    // Page 16
+    "Days turned into weeks, and weeks into months. Life returned to normal for Gigi, but she often found herself gazing up at the moon, reminiscing about her adventure. The moonstone Alaric gave her always hung around her neck, glowing softly.",
+    
+    // Page 17
+    "One evening, as Gigi lay on her favorite hill, she held the moonstone close to her heart. To her surprise, it began to glow brightly, and she could hear Alaric's voice, singing a lullaby from the moon.",
+    
+    // Page 18
+    "The gentle song reminded Gigi that no matter the distance, true friendships never fade. Every time she missed Alaric, she'd listen to the moonstone's song, and it felt like he was right beside her.",
+    
+    // Page 19
+    "Gigi’s adventure became a legendary tale in the savannah. Animals from far and wide would come to hear about her journey to the moon and her alien friend. And every time she narrated her story, the moonstone would glow a little brighter.",
+    
+    // Page 20
+    "And so, in the heart of the savannah, under the silver glow of the moon, Gigi’s tale became a beacon of hope, wonder, and the magic that awaits when one dares to dream. And every dreamer on Earth knew that adventures were waiting, just a dream away."
 ]
+
 
 
 // Function to split a story into pages (Not required for the defaultStoryPages as it's already split, but useful for other stories)
@@ -396,6 +414,3 @@ struct AttributedText: UIViewRepresentable {
         uiView.attributedText = attributedString
     }
 }
-
-
-
